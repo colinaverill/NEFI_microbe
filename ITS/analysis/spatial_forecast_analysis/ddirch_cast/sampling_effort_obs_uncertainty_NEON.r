ca9fdcfa6e281545e2fc71e6796887fa03ee0085
colin.averill <colin.averill@gmail.com>
2019-01-07 14:22:22 -0500

    Modified sampling effort analysis to consider all sites.

--- ITS/analysis/spatial_forecast_analysis/ddirch_cast/sampling_effort_obs_uncertainty_NEON.r
+++ ITS/analysis/spatial_forecast_analysis/ddirch_cast/sampling_effort_obs_uncertainty_NEON.r
@@ -16,42 +16,58 @@ registerDoParallel(cores=n.cores)
 dat <- readRDS(NEON_ITS_fastq_taxa_fg.path)
 dat <- dat$abundances
 dat$site <- substr(dat$geneticSampleID, 1, 4)
+all.dat <- dat
+sites <- unique(dat$site)
 dat <- dat[dat$site == 'HARV',]
 dat <- dat[,c('other','Ectomycorrhizal','Arbuscular','Saprotroph','Pathogen')]
 dat <- (dat + 1)
 dat <- dat/rowSums(dat)
 
 #sampling depths and number of trials.
-n.samp <- c(3,5,6, 8, 10, 15, 20, 30, 40, 50)
-n.trial <- 1000
+potential.n.samp <- c(3,5,6, 8, 10, 15, 20, 30, 40, 50)
+n.trial <- 5
 
 #run simulation.----
-super.out <- 
-  foreach(j = 1:n.trial) %dopar% {
-    output <- list()
-    for(i in 1:length(n.samp)){
-      #sample your dataset.
-      sample <- data.frame(dat[sample(nrow(dat), size = n.samp[i], replace = F),])
-      sample$Y <- DR_data(sample[,1:ncol(sample)])
-      #fit dirichlet interecept.
-      mod <- DirichReg(Y ~ 1, data = sample)
-      mult <- sum(mod$fitted.values$alpha[1,])
-      output[[i]] <- mult
+cat(paste0('Running bootstrap simulation for ',n.trials,' iterations...\n'))
+tic()
+super.super.out <- list()
+for(k in 1:length(sites)){
+  dat <- all.dat[all.dat$site == sites[k],]
+  dat <- dat[,c('other','Ectomycorrhizal','Arbuscular','Saprotroph','Pathogen')]
+  dat <- (dat + 1)
+  dat <- dat/rowSums(dat)
+  n.samp <- potential.n.samp[potential.n.samp <= nrow(dat)]
+  super.out <- 
+    foreach(j = 1:n.trial) %dopar% {
+      output <- list()
+      for(i in 1:length(n.samp)){
+        #sample your dataset.
+        sample <- data.frame(dat[sample(nrow(dat), size = n.samp[i], replace = F),])
+        sample$Y <- DR_data(sample[,1:ncol(sample)])
+        #fit dirichlet interecept.
+        mod <- DirichReg(Y ~ 1, data = sample)
+        mult <- sum(mod$fitted.values$alpha[1,])
+        output[[i]] <- mult
+      }
+      output <- unlist(output)
+      return(output)
     }
-    output <- unlist(output)
-    return(output)
+  super.out <- do.call(rbind, super.out)
+  colnames(super.out) <- n.samp
+  #expand super.out into 2 column- y and n.samp.
+  y <- as.vector(super.out)
+  lab <- list()
+  for(i in 1:length(n.samp)){
+    lab[[i]] <- rep(n.samp[i],n.trial)
   }
-super.out <- do.call(rbind, super.out)
-colnames(super.out) <- n.samp
-#expand super.out into 2 column- y and n.samp.
-y <- as.vector(super.out)
-lab <- list()
-for(i in 1:length(n.samp)){
-  lab[[i]] <- rep(n.samp[i],n.trial)
+  lab <- unlist(lab)
+  super.out2 <- data.frame(y, lab)
+  colnames(super.out2) <- c('mu','n.samp')
+  super.super.out[[k]] <- super.out2
+  cat(paste0(k,' of ',length(sites),' sites fitted.\n'))
 }
-lab <- unlist(lab)
-super.out2 <- data.frame(y, lab)
-colnames(super.out2) <- c('mu','n.samp')
+names(super.super.out) <- sites
+cat('Simulation complete.');toc()
 
 #Save output for downstream analysis.----
-saveRDS(super.out2, output.path)
+saveRDS(super.super.out, output.path)
